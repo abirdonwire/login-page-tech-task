bd076001bbe7aa9845e9a42fea5f7eaa9a4f238e
Ellie Maidment <ellie947@hotmail.com>
2024-03-01 11:35:36 +0000

    create Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+
+export const Footer: React.FC = () => {
+  return (
+    <footer>
+      <address>
+        <p>
+          For access and support please email{" "}
+          <a href="#" aria-label="Email DCM support">
+            support@DCM.com
+          </a>
+        </p>
+      </address>
+
+      <p>DCM Origination Technologies Ltd 2023</p>
+    </footer>
+  );
+};
