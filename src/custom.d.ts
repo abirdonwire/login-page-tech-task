ec99bf4cfc9a9bb9d5e4a8126cb8cb4af3e21a7b
Ellie Maidment <ellie947@hotmail.com>
2024-03-01 11:53:38 +0000

    declare css modules

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -4,3 +4,7 @@ declare module "*.svg" {
   const src: string;
   export default src;
 }
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
